ea83772ad78df72a1f2319b16b64462bc0f5d5a0
Antonio-Zago <antoniozagodev@gmail.com>
2021-10-02 15:00:54 -0300

    Criação da View de exibição da lista de lanches através do Scaffold

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,27 @@
+﻿using LanchesMac.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public LancheController(ILancheRepository lancheRepository, ICategoriaRepository categoriaRepository) //Como IlancheRepository e IcategoriaRepository estão registrados como serviços, na chamada do controlador, dará uma instancia de LancheRepository e CategoriaRepository
+        {
+            _lancheRepository = lancheRepository;
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
