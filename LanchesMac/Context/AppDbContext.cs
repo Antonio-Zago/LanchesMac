cbf4d7f94dd7c3b63930bcac501656a42b5a0339
Antonio-Zago <antoniozagodev@gmail.com>
2021-10-03 09:18:05 -0300

    Uso do migration para criação da tabela CarrinhoDeCompraItens

--- LanchesMac/Context/AppDbContext.cs
+++ LanchesMac/Context/AppDbContext.cs
@@ -14,5 +14,7 @@ namespace LanchesMac.Context
         public DbSet<Lanche> Lanches { get; set; }
 
         public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
     }
 }
---
+++ LanchesMac/Models/CarrinhoCompraItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesMac.Models
+{
+    public class CarrinhoCompraItem
+    {
+        public int CarrinhoCompraItemId { get; set; }
+
+        public Lanche Lanche { get; set; } //Chave estrangeira da tabela lanches
+
+        public int Quantidade { get; set; }
+
+        [StringLength(200)]
+        public string CarrinhoCompraId { get; set; }
+    }
+}
